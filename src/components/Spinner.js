20e6d96a3f0501eb2bc2894c466915e820ae08de
MattRuetz <matt.b.ruetz@gmail.com>
2022-03-22 19:43:12 -0400

    private routes for logged in users added with componenet to protect a route. Added randomized text to spinner component

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,3 +1,6 @@
+// loading messages
+import msgs from '../data/messages.json';
+
 function Spinner() {
     return (
         <>
@@ -9,6 +12,10 @@ function Spinner() {
                     position: 'relative',
                 }}
             >
+                <h2 className="loading-text">
+                    <em>{msgs[Math.floor(Math.random() * 30)]}</em>
+                </h2>
+
                 <img
                     src="https://media1.giphy.com/media/8cARKvZXq73sOZBdrw/giphy.gif?cid=790b76116b41cd634f146f0bb5e57e9c4e758675c22c7844&rid=giphy.gif&ct=s"
                     width="30%"
