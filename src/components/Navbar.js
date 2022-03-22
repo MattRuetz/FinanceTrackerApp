44df0bbc86ba590c9b49c8bfacb7fc60ee8ae447
MattRuetz <matt.b.ruetz@gmail.com>
2022-03-22 17:04:01 -0400

    plugged hook into login page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom';
 import { useLogout } from '../hooks/useLogout';
+import { useAuthContext } from '../hooks/useAuthContext';
 import styles from './Navbar.module.css';
 
 function Navbar() {
     const { logout } = useLogout();
+    const { user } = useAuthContext();
 
     return (
         // classes ref'd from CSS module... MUST ADD CLASSES LIKE THIS
@@ -12,19 +14,25 @@ function Navbar() {
                 <li className={styles.title}>
                     <Link to="/">myMoney</Link>
                 </li>
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-                <li>
-                    <Link to="/signup">
-                        <strong>Signup!</strong>
-                    </Link>
-                </li>
-                <li>
-                    <button className="btn" onClick={logout}>
-                        Logout
-                    </button>
-                </li>
+                {/* display relevant buttons for logged in OR not */}
+                {user ? (
+                    <li>
+                        <button className="btn" onClick={logout}>
+                            Logout
+                        </button>
+                    </li>
+                ) : (
+                    <>
+                        <li>
+                            <Link to="/login">Login</Link>
+                        </li>
+                        <li>
+                            <Link to="/signup">
+                                <strong>Signup!</strong>
+                            </Link>
+                        </li>
+                    </>
+                )}
             </ul>
         </nav>
     );
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,4 +1,8 @@
 import { useState } from 'react';
+// hooks
+import { useLogin } from '../../hooks/useLogin';
+// components
+import Spinner from '../../components/Spinner';
 // styles
 import styles from './Login.module.css';
 
@@ -6,33 +10,47 @@ function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    // fn, state, state
+    const { login, error, isPending } = useLogin();
+
     const handleSubmit = (e) => {
         e.preventDefault();
         // Authenticate login attempt
+        login(email, password);
     };
 
     return (
-        // If module classnames have cant be ref'd with styles.login-form (because of -) do this
-        <form className={styles['login-form']}>
-            <h2>Login</h2>
-            <label>
-                <span>email:</span>
-                <input
-                    type="email"
-                    onChange={(e) => setEmail(e.target.value)}
-                    value={email}
-                />
-            </label>
-            <label>
-                <span>password:</span>
-                <input
-                    type="password"
-                    onChange={(e) => setPassword(e.target.value)}
-                    value={password}
-                />
-            </label>
-            <button className="btn">Login</button>
-        </form>
+        <>
+            {isPending ? (
+                <Spinner />
+            ) : (
+                // If module classnames have cant be ref'd with styles.login-form (because of -) do this
+                <form className={styles['login-form']}>
+                    <h2>Login</h2>
+                    <label>
+                        <span>email:</span>
+                        <input
+                            type="email"
+                            onChange={(e) => setEmail(e.target.value)}
+                            value={email}
+                        />
+                    </label>
+                    <label>
+                        <span>password:</span>
+                        <input
+                            type="password"
+                            onChange={(e) => setPassword(e.target.value)}
+                            value={password}
+                        />
+                    </label>
+                    {error ? (
+                        <p>{error}</p>
+                    ) : (
+                        <button onClick={handleSubmit} className="btn">Login</button>
+                    )}
+                </form>
+            )}
+        </>
     );
 }
 
